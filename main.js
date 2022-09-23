9218413a5d2c2ed535e39e0f27bf37a695baf24e
AaryanTeckchandani <96259203+AaryanTeckchandani@users.noreply.github.com>
2022-09-23 17:08:22 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-var studentArray=[];
-function submit(){
-    var name1=document.getElementById("nameOfPerson1").value;
+var studentArray = [];
+
+function submit() {
+    console.log('in submit');
+    var name1 = document.getElementById("nameOfPerson1").value;
     studentArray.push(name1);
-    studentArray.sort();
-    document.getElementById("result").innerHTML=studentArray;
+
+
 }
+
+function sort() {
+    studentArray.sort();
+    document.getElementById("result").innerHTML = studentArray;
+    var removeCommas = studentArray.join(" ");
+    document.getElementById("displayNameWithoutCommas").innerHTML = removeCommas;
+
+}
+
+
+
+
+
+
+
+
+
+
